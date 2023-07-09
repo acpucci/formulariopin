fcb9c2690ac5347a177d3cf3b2f5ef4bb0934044
Andrea Pucci <andreacpucci@gmail.com>
2023-07-09 19:57:35 -0300

    formulario finalizado con scss

--- src/Views/Contact/Components/Formulario.jsx
+++ src/Views/Contact/Components/Formulario.jsx
@@ -1,76 +1,91 @@
 import React from 'react'
+import './style.scss'
+import contimg from '../img/contact-image.png'
 
 function Formulario(props) {
   return (
-    <>
-      <form onSubmit={props.handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            placeholder="Name"
-            onChange={props.handleChange}
-            value={props.newUser.name}
+    <section>
+      <div className="contact-wrapper" id="contacto">
+        <div className="contact-form">
+          <h1 className="titulo">Get in touch <br />
+            <span>We are hiring!</span></h1>
+          <form onSubmit={props.handleSubmit}>
+            <div>
+              <label htmlFor="name">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Name"
+                maxLength="40"
+                onChange={props.handleChange}
+                value={props.newUser.name}
 
-          />
+              />
+            </div>
+            <div className="separador">
+              <label htmlFor="email">
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="e-mail"
+                maxLenght="60"
+                aria-describedby="emailHelp"
+                onChange={props.handleChange}
+                value={props.newUser.email}
+              />
+            </div>
+            <div className="separador">
+              <label htmlFor="phone">
+                Phone
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                placeholder="Phone number"
+                maxLenght="12"
+                onChange={props.handleChange}
+                value={props.newUser.phone}
+              />
+            </div>
+            <div className="separador-mensaje">
+              <label htmlFor="message">
+                Message
+              </label>
+              <input
+                type="text-area"
+                id="message"
+                placeholder="Please, leave your message"
+                maxLenght="500"
+                onChange={props.handleChange}
+                value={props.newUser.message}
+              />
+            </div>
+            <div className="boton">
+              <button disabled={props.disableSubmit} type="submit" className="botoncito">
+                Send
+              </button>
+            </div>
+            {props.success &&
+              <div className="mensaje-exito" role="alert">
+                Thank you! Your information has been received!
+              </div>}
+            {props.error &&
+              <div className="mensaje-error" role="alert">
+                {props.errorMessage}
+              </div>}
+          </form>
         </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="e-mail"
-            aria-describedby="emailHelp"
-            onChange={props.handleChange}
-            value={props.newUser.email}
-          />
+
+        <div>
+          <img src={contimg} alt="" />
         </div>
-        <div className="mb-3">
-          <label htmlFor="phone" className="form-label">
-            Phone
-          </label>
-          <input
-            type="tel"
-            className="form-control"
-            id="phone"
-            placeholder="Phone number"
-            onChange={props.handleChange}
-            value={props.newUser.phone}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="message" className="form-label">
-            Message
-          </label>
-          <input
-            type="text-area"
-            className="form-control"
-            id="message"
-            placeholder="Please leave your message"
-            onChange={props.handleChange}
-            value={props.newUser.message}
-          />
-        </div>
-        <button disabled={props.disableSubmit} type="submit" className="btn btn-primary">
-          Send
-        </button>
-      </form>
-      {props.success &&
-        <div className="alert alert-success m-3" role="alert">
-          Thank you! Your information has been received!
-        </div>}
-      {props.error &&
-        <div className="alert alert-danger m-3" role="alert">
-          {props.errorMessage}
-        </div>}
-    </>
+      </div>
+    </section>
+
   );
 }
 
